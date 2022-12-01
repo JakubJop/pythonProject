7ad0b436e8667c560bfa108de560e27f931fc4b5
w67238 <kuba_04042@interia.pl>
2022-12-01 18:19:17 +0100

    zad1lab7

---
+++ lab7.py
@@ -0,0 +1,10 @@
+lista = []
+for i in range(7, -1, -1):
+    lista.append(2**i)
+    print(lista)
+    lista1 = [2**i
+              for i in range(7, -1, -1)]
+    wagi = np.array(lista1)
+liczba_bin = np.random.randint(low=0, high=2, size=8)
+print(liczba_bin)
+tab = wagi * liczba_bin print(tab.sum())
